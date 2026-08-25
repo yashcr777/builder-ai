7b0e4757f9436356b07f9cfa7a863b86afe8cb86
yashcr777 <yashagarwalnobody@gmail.com>
2026-08-25 15:16:14 +0530

    some more changes in backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,7 +9,7 @@ const setSessionCookie = (res, payload)=>{
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         path: "/",
     })
@@ -82,7 +82,7 @@ export async function logout(_req, res){
     res.cookie("token", "", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 0,
         path: "/",
     })
